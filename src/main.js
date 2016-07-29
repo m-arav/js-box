84f4406e40f5221f328dfa651240a92cee3b16ec
Aravind M <murali.aravind94@gmail.com>
2016-07-29 15:37:55 +0530

    Add Logic for movement

--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
-console.log("main.js");
+var move = {
+  37: function() {
+        //left arrow key
+        $(".box").css({
+          left: "-=30"
+        });
+      },
+  38: function() {
+        //up arrow key
+        $(".box").css({
+          top: "-=30"
+        });
+      },
+  39: function() {
+        //right arrow key
+        $(".box").css({
+          left: "+=30"
+        });
+      },
+  40: function() {
+        //bottom arrow key
+        $(".box").css({
+          top: "+=30"
+        });
+      }
+}
 
 $(document).ready(function(){
-  $(document).on('keydown', function(){
-    console.log('keydown');
-  });
+  $(document).on('keydown', function(event){
 
+    console.log("moving");
+
+    move[event.keyCode]();
+
+    a = $(".box").position();
+    console.log(a);
+
+  });
 });
