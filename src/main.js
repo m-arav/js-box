d037ddc219f448dfef217fbe56dd2f2625f1b7bc
Aravind M <murali.aravind94@gmail.com>
2016-07-29 18:32:07 +0530

    Add Validate Function
    Notes :
    - Validates the next move
    - Validation is based on the parent container that the box is inside

--- src/main.js
+++ src/main.js
@@ -25,15 +25,52 @@ var move = {
       }
 }
 
+function validateMove(event, viewPortHight, viewPortWidth, currentPosition) {
+
+  var direction = event.keyCode;
+
+  console.log(direction ,viewPortWidth , viewPortHight, currentPosition);
+
+  switch (direction){
+    case 38: // UP
+
+          if ( currentPosition.top - 30 > 0){
+              return true;
+          }
+          return false;
+  case 40: // down
+
+        if ((viewPortHight-100) > currentPosition.top + 30){
+          //console.log('Hieght :'+viewPortHight+ 'against '+ currentPosition.top);
+          return true;
+        }
+        return false;
+  case 37: // left
+          if ( currentPosition.left - 30 > 10){
+                return true;
+          }
+          return false;
+
+  case 39: // right
+          if ((viewPortWidth-100) > currentPosition.left + 30){
+                return true;
+          }
+          return false;
+  }
+}
+
 $(document).ready(function(){
   $(document).on('keydown', function(event){
 
-    console.log("moving");
+    var viewPortHight = $('.container').height();
+    var viewPortWidth = $('.container').width();
+    var currentPosition = $(".box").position();
 
-    move[event.keyCode]();
+    event.preventDefault();
 
-    a = $(".box").position();
-    console.log(a);
+    if(validateMove(event, viewPortHight, viewPortWidth, currentPosition)){
+      move[event.keyCode]();
+    }
 
   });
 });
